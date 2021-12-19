9074f47764167dd5ac378e6fcee856c5a56aa5e8
your name <your email>
2021-12-19 17:42:07 +0800

    cfvg

--- bookManageFrontDev/src/router/index.js
+++ bookManageFrontDev/src/router/index.js
@@ -11,36 +11,29 @@ const router = new Router({
         component: login,
         children: [
 
-        ]},
-        {
-            path: "/home",
-            component: adminHome,
-        
+        ]
+    },
+    {
+        path: "/home",
+        component: adminHome,
+
     }]
 });
 
 // 导航守卫
-<<<<<<< HEAD
-// 使用 router.beforeEach 注册一个全局前置守卫，判断用户是否登陆。
-router.beforeEach((to, from, next) => {
-    if (to.path === '/login') {
-        next();
-    } else {
-        let token = localStorage.getItem('Authorization');
-=======
 // 使用 router.beforeEach 注册一个全局前置守卫，判断用户是否登陆
 // router.beforeEach((to, from, next) => {
 //     if (to.path === '/login') {
 //         next();
 //     } else {
 //         let token = localStorage.getItem('Authorization');
->>>>>>> a50b18427bccd781cfba1e6163ea5bc245c847d8
 
-//         if (token === null || token === '') {
-//             next('/login');
-//         } else {
-//             next();
-//         }
-//     }
+        //         if (token === null || token === '') {
+        //             next('/login');
+        //         } else {
+        //             next();
+        //         }
+        //     }
+    // }
 // });
 export default router;
