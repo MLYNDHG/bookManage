4ae46674cf9930c1558ea5f83dee24799d3ae568
Tusir <tuwuei@gmail.com>
2021-12-21 19:34:45 +0800

    ddd

--- bookManageFrontDev/src/views/admin/bookManage.vue
+++ bookManageFrontDev/src/views/admin/bookManage.vue
@@ -19,8 +19,8 @@
         class="filter-item"
         v-model="queryInfo.condition.description"
       ></el-input>
-      <el-button class="dalfBut">查询</el-button>
-      <el-button type="danger" class="butT">添加图书</el-button>
+      <el-button class="dalfBut" @click="bookSearch">查询</el-button>
+      <el-button type="danger" class="butT" @click="addBookBto">添加图书</el-button>
     </div>
 
     <!-- table区域 -->
@@ -35,10 +35,25 @@
           >借阅</el-button
         ></el-table-column
       >
+
+      <el-table-column label="操作" header-align="center" align="center">
+        <template slot-scope="scope">
+          <el-button size="small" @click="showEditDialog(scope.row.id)"
+            >编辑</el-button
+          >
+          <el-button
+            size="small"
+            type="danger"
+            @click="deleteBook(scope.row.bid)"
+            >删除</el-button
+          >
+        </template>
+      </el-table-column>
     </el-table>
 
     <!-- 分页 -->
     <el-pagination
+      @size-change="sizeChange"
       @current-change="currentChange"
       :current-page="queryInfo.pageCondition.pageNo"
       :page-sizes="[3, 6, 9]"
@@ -47,15 +62,69 @@
       :total="total"
     >
     </el-pagination>
+
+    <!-- 新建图书对话框 -->
+    <el-dialog
+      title="新增图书"
+      :visible.sync="addBookFormDialog"
+      @close="addBookClose"
+    >
+      <el-form :model="addBookForm" ref="addBookRef">
+        <el-form-item label="书籍名称" label-width="100px">
+          <el-input v-model="addBookForm.name" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="作者" label-width="100px">
+          <el-input v-model="addBookForm.author" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="描述" label-width="100px">
+          <el-input
+            v-model="addBookForm.description"
+            autocomplete="off"
+          ></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="addBookFormDialog = false">取 消</el-button>
+        <el-button type="primary" @click="addBook">确 定</el-button>
+      </div>
+    </el-dialog>
+
+    <!-- 编辑图书对话框 -->
+    <el-dialog title="新建图书" :visible.sync="editBookDialog">
+      <el-form :model="editBookList">
+        <el-form-item label="书籍名称" label-width="100px">
+          <el-input
+            v-model="editBookList.name"
+            autocomplete="off"
+            disabled
+          ></el-input>
+        </el-form-item>
+        <el-form-item label="作者" label-width="100px">
+          <el-input v-model="editBookList.author" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="描述" label-width="100px">
+          <el-input
+            v-model="editBookList.description"
+            autocomplete="off"
+          ></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="editBookDialog = false">取 消</el-button>
+        <el-button type="primary" @click="editBook">确 定</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 <script>
-import { selectBookList } from "@/services/bookcontroller.js";
+import { selectBookList,saveBook } from "@/services/bookcontroller.js";
 import requestPageData from "@/model/RequestPageData.js";
+import addBookForm from "@/model/addBookForm.js";
 export default {
   data() {
     return {
       requestPageData: new requestPageData(),
+      addBookForm: new addBookForm(),
       //获取图书列表的参数对象
       queryInfo: {
         condition: {
@@ -82,6 +151,15 @@ export default {
 
       //图书信息列表
       bookList: [],
+
+      //新增图书对话框
+      addBookFormDialog: false,
+
+      //编辑图书对话框
+      editBookDialog: false,
+
+      //编辑图书展示信息
+      editBookList: []
     };
   },
 
@@ -90,7 +168,7 @@ export default {
   },
 
   methods: {
-    //查询
+    //条件查询
     getAll() {
       this.requestPageData.condition = this.queryInfo.condition;
       this.requestPageData.pageCondition = this.queryInfo.pageCondition;
@@ -99,19 +177,78 @@ export default {
         .then((res) => {
           console.log(res);
           this.bookList = res.data.resultPages;
-          this.total = res.data.totalRows
+          this.total = res.data.totalRows;
         })
         .catch((res) => {
           console.log(res);
         });
     },
 
-    //监听页码改变事件
+    //匹配查询
+    bookSearch() {
+      this.getAll();
+    },
+
+    //监听当前页码改变事件
     currentChange(page) {
       this.queryInfo.pageCondition.pageNo = page;
 
-      this.getAll()
+      this.getAll();
     },
+
+    //监听页码大小改变事件
+    sizeChange(pageSize) {
+      //console.log(a)
+      this.queryInfo.pageCondition.pageSize = pageSize;
+      this.getAll();
+    },
+
+    //新增图书
+    addBook() {
+        saveBook(this.addBookForm).then(res=>{
+            console.log(res)
+            this.addBookFormDialog = false
+            this.$message({ message: "添加成功", type: "success" });
+        }).catch(res=>{
+            console.log(res)
+        })
+    },
+
+    //点击弹出新增对话框
+    addBookBto() {
+        this.addBookFormDialog = true
+    },
+
+    //监听新增对话框关闭
+    addBookClose() {
+        this.$refs.addBookRef.resetFields()
+    },
+
+    //点击编辑展示编辑信息
+    showEditDialog(bid) {
+        this.requestPageData.condition.id = bid
+        selectBookList(this.requestPageData).then(res=>{
+            //console.log(res)
+            this.editBookList = res.data.resultPages[0];
+            this.requestPageData.condition.id = ""
+        }).catch(res=>{
+            console.log(res)
+        })
+        this.editBookDialog = true
+        console.log(bid)
+    },
+
+    //点击修改编辑信息
+    editBook() {
+        saveBook(this.editBookList).then(res=>{
+            console.log(res)
+            this.editBookDialog = false
+            this.$message({ message: "修改成功", type: "success" });
+            this.getAll()
+        }).catch(res=>{
+            console.log(res)
+        })
+    }
   },
 };
 </script>
@@ -125,14 +262,13 @@ export default {
     margin: 5px 8px;
   }
 
-
   .el-pagination {
-      margin-top: 8px;
+    margin-top: 8px;
     margin-right: 25px;
   }
 }
-      .el-tabs__nav-scroll {
-        overflow: hidden;
-        border-bottom: solid;
-      }
+.el-tabs__nav-scroll {
+  overflow: hidden;
+  border-bottom: solid;
+}
 </style>
