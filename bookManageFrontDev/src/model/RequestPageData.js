e5f0cd5a48f3c09e77c84a5701f57c48fb957ccb
Tusir <tuwuei@gmail.com>
2021-12-22 00:16:28 +0800

    fsd

--- bookManageFrontDev/src/model/RequestPageData.js
+++ bookManageFrontDev/src/model/RequestPageData.js
@@ -3,15 +3,9 @@
  @param pageSize:class java.lang.Integer
 */
 class pageCondition {
-<<<<<<< HEAD
     constructor() {//ES6构造器
         this.pageNo = 0
         this.pageSize = 0
-=======
-    constructor() {
-        this.pageNo = ''
-        this.pageSize = ''
->>>>>>> 4ae46674cf9930c1558ea5f83dee24799d3ae568
     }
 }
 
@@ -22,10 +16,6 @@ class pageCondition {
 export default class RequestPageData {
     constructor() {
         this.pageCondition = new pageCondition();
-<<<<<<< HEAD
         this.condition = '';
-=======
-        this.condition = ''
->>>>>>> 4ae46674cf9930c1558ea5f83dee24799d3ae568
     }
 }
