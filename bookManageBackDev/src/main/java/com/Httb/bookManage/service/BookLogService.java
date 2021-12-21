52c27f1904b87edc2ef8dd27ee423c876b1abd17
王双宝 <15172679024@163.com>
2021-12-21 14:53:36 +0800

    借阅的增删改查完成1.0

---
+++ bookManageBackDev/src/main/java/com/Httb/bookManage/service/BookLogService.java
@@ -0,0 +1,74 @@
+package com.Httb.bookManage.service;
+
+import com.Httb.bookManage.dao.BookExtDao;
+import com.Httb.bookManage.dao.BookLogExtDao;
+import com.Httb.bookManage.mbg.entity.Book;
+import com.Httb.bookManage.mbg.entity.BookLog;
+import com.Httb.bookManage.model.BookLogVO;
+import com.Httb.bookManage.model.BookVO;
+import com.Httb.bookManage.util.DateUtil;
+import com.Httb.bookManage.util.RequestPageData;
+import com.Httb.bookManage.util.ResponsePageData;
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class BookLogService {
+
+    // 图书
+    @Resource
+    private BookExtDao bookExtDao;
+
+    // 借阅
+    @Resource
+    private BookLogExtDao bookLogExtDao;
+
+    /**
+     * 借阅列表
+     */
+    public ResponsePageData<BookLogVO> selectBookLogList(RequestPageData<BookLogVO> requestPageData) {
+        // 分页查询
+        Page<Object> page = PageHelper.startPage(requestPageData.getPageCondition().getPageNo(),requestPageData.getPageCondition().getPageSize());
+        List<BookLogVO> bookLogVOS = bookLogExtDao.selectBookLogList(requestPageData.getCondition());
+        for (BookLogVO b:
+             bookLogVOS) {
+
+            // 时间格式化
+            b.setStartTime(b.getTimestart());
+            if (b.getTimeend() != null) {
+                b.setEndTime(b.getTimeend());
+            }
+        }
+        return new ResponsePageData<>(page);
+    }
+
+    /**
+     * save 借阅记录
+     * 新增借阅 还书
+     */
+    public Integer saveBookLog(BookLogVO bookLogVO) {
+        // 判断图书是否已借阅
+        Book book = bookExtDao.selectByPrimaryKey(bookLogVO.getBid());
+        if (book.getLog()) {
+            // 图书已借阅，还书
+            // 修改借阅记录的timeend
+            bookLogVO.setTimeend(new Date());
+            bookLogExtDao.updateByPrimaryKeySelective(bookLogVO);
+            // 修改图书的log为false
+            book.setLog(false);
+        } else {
+            // 借阅图书
+            // 添加借阅记录
+            bookLogVO.setTimestart(new Date());
+            bookLogExtDao.insert(bookLogVO);
+            // 修改图书的log为true
+            book.setLog(true);
+        }
+        return bookExtDao.updateByPrimaryKeySelective(book);
+    }
+}
