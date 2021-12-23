96d459b9c6fc8e238e4a0131689410c29d01b40c
your name <your email>
2021-12-23 14:58:31 +0800

    11

--- bookManageFrontDev/src/services/UserController.js
+++ bookManageFrontDev/src/services/UserController.js
@@ -3,29 +3,14 @@ import request from '@/utils/request'
 /*
 *@param: file:interface org.springframework.web.multipart.MultipartFile
 */
-<<<<<<< HEAD
-export function uploadImage(file) {
-    return request({
-        url: request.baseUrl + `/uploadImage`,
-=======
 export function saveUser(user) {
     return request({
         url: request.baseUrl + `/saveUser`,
->>>>>>> 3f86c140374b015f225d05d7a82fba088df27bbc
         method: 'POST',
         data:file
     })
 }
 /*
-<<<<<<< HEAD
-*@param: requestPageData:com.Httb.bookManage.util.RequestPageData<com.Httb.bookManage.mbg.entity.User>
-*/
-export function selectUserList(requestPageData) {
-    return request({
-        url: request.baseUrl + `/selectUserList`,
-        method: 'POST',
-        data:requestPageData
-=======
 *@param: file:interface org.springframework.web.multipart.MultipartFile
 */
 export function uploadImage(file) {
@@ -33,7 +18,6 @@ export function uploadImage(file) {
         url: request.baseUrl + `/uploadImage`,
         method: 'POST',
         data:file
->>>>>>> 3f86c140374b015f225d05d7a82fba088df27bbc
     })
 }
 /*
@@ -47,7 +31,6 @@ export function login(user) {
     })
 }
 /*
-<<<<<<< HEAD
 *@param: userVO:class com.Httb.bookManage.model.UserVO
 */
 export function updatePassword(userVO) {
@@ -55,15 +38,6 @@ export function updatePassword(userVO) {
         url: request.baseUrl + `/updatePassword`,
         method: 'POST',
         data:userVO
-=======
-*@param: user:class com.Httb.bookManage.mbg.entity.User
-*/
-export function login(user) {
-    return request({
-        url: request.baseUrl + `/login`,
-        method: 'POST',
-        data:user
->>>>>>> 3f86c140374b015f225d05d7a82fba088df27bbc
     })
 }
 /*
